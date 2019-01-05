1393675fe03792f56487e1b86d3ed860fc896618
Ross Rotaru <rostislav@rotaru.me>
2019-01-04 17:08:46 -0800

    Modified path

--- SourceCodes/Program.ts
+++ SourceCodes/Program.ts
@@ -1,6 +1,6 @@
 import { WebHost } from "./Packages/DotType.Hosting/WebHost";
 import { Startup } from "./Startup";
-import { FaviconParser } from "./Packages/DotType.WebServer.FaviconMiddleware/FaviconParser";
+import { FaviconParser } from "./Packages/DotType.WebServer.Favicon/FaviconParser";
 import "./Packages/DotType.WebServer/Extensions/DotType.WebServer.WebHostExtensions";
 import "./Packages/DotType.Mvc/Extensions/DotType.Mvc.WebApplicationExtensions";
 
