06b35438441190ee702455c46b7918ebb8d44808
Ross Rotaru <rostislav@rotaru.me>
2019-01-05 19:56:03 -0800

    Image sizes

--- SourceCodes/Controllers/Home.ts
+++ SourceCodes/Controllers/Home.ts
@@ -5,7 +5,7 @@ export class Home extends Controller
 {
     public async Index(id: string): Promise<IActionResult>
     {
-        await this.HttpContext.Response.WriteAsync("This is a text from index action in home controller.");
+        await this.HttpContext.Response.WriteAsync("Welcome to DotType!");
         return this.Ok();
     }
 }
